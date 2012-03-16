ecbca3152e20ad0457cd182ed40f5b0f82bec5f8
Matt Lambie <mlambie@lambie.org>
2012-03-16 18:46:06 +0800

    v.0.1 release

--- dist-upgrade-all.rb
+++ dist-upgrade-all.rb
@@ -4,6 +4,14 @@ require 'rubygems'
 require 'shellwords'
 require 'appscript'
 
+class Host
+  attr_reader :name, :login
+  def initialize(name, login = 'mlambie')
+    @name = name
+    @login = login
+  end
+end
+
 class Terminal
   include Appscript
   attr_reader :terminal, :current_window
@@ -18,7 +26,6 @@ class Terminal
     run command
   end
 
-  private
   def run(command)
     command = command.shelljoin if command.is_a?(Array)
     if command && !command.empty?
@@ -27,8 +34,28 @@ class Terminal
   end
 end
 
+def hosts
+  hosts = Array.new
+  servers = %w{arcee astrotrain blaster blitzwing broadside cliffjumper ironhide jazz laserbeak mirage prime prowl ravage rumble}
+  servers += %w{bizox csd cw-production cw-staging imeducate imports101 mg synaptor tapdoctor}
+  servers += %w{ilca-lb ilca-db ilca-web-01 ilca-web-02}
+  servers.each do |s|
+    hosts << Host.new("#{s}.thefrontiergroup.net.au") 
+  end
+  6.times {|x| hosts << Host.new("ipv-app-0#{x + 1}.thefrontiergroup.net.au", 'ipv') }
+  hosts << Host.new('mail.ilc.com.au')
+end
+
+first = true
 Terminal.new do |t|
-  t.tab("htop && exit")
-  t.tab("htop && exit")
-  t.tab("htop && exit")
+  hosts.each do |h|
+    sleep 1
+    cmd = "ssh #{h.login}@#{h.name} -t 'sudo aptitude update && sudo aptitude dist-upgrade -y && sudo aptitude clean && exit' && exit"
+    if first == true
+      t.tab(cmd, 'n')
+      first = false
+    else
+      t.tab(cmd)
+    end
+  end
 end
